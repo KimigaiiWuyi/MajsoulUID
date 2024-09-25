abf71370db60ad3b27dcb064e02c00b450757f6e
baiqwerdvd <158065462+baiqwerdvd@users.noreply.github.com>
2024-09-25 16:05:36 +0800

    fix

--- MajsoulUID/majs_notify/majsoul.py
+++ MajsoulUID/majs_notify/majsoul.py
@@ -1,32 +1,32 @@
+import asyncio
 import json
+import random
 import time
 import uuid
-import random
-import asyncio
-from typing import cast
 from collections.abc import Iterable
+from typing import cast
 
 import websockets.client
-from msgspec import convert
-from httpx import AsyncClient
 from gsuid_core.gss import gss
 from gsuid_core.logger import logger
+from httpx import AsyncClient
+from msgspec import convert
 
 from ..lib import lq as liblq
-from .codec import MajsoulProtoCodec
-from .majsoul_friend import MajsoulFriend
 from ..utils.api.remote import PlayerLevel
-from .utils import getRes, encodeAccountId
-from .constants import HEADERS, ModeId2Room
 from ..utils.database.models import MajsPush, MajsUser
+from .codec import MajsoulProtoCodec
+from .constants import HEADERS, ModeId2Room
+from .majsoul_friend import MajsoulFriend
 from .model import (
     MajsoulConfig,
-    MajsoulResInfo,
+    MajsoulDecodedMessage,
     MajsoulLiqiProto,
+    MajsoulResInfo,
     MajsoulServerList,
     MajsoulVersionInfo,
-    MajsoulDecodedMessage,
 )
+from .utils import encodeAccountId, getRes
 
 PP_HOST = "https://game.maj-soul.com/1/?paipu="
 
@@ -43,9 +43,7 @@ class MajsoulConnection:
         self._ws = None
         self._req_events: dict[int, asyncio.Event] = {}
         self._res: dict[int, MajsoulDecodedMessage] = {}
-        self.clientVersionString = "web-" + versionInfo.version.replace(
-            ".w", ""
-        )
+        self.clientVersionString = "web-" + versionInfo.version.replace(".w", "")
         self.no_operation_counter = 0
         self.bg_tasks = []
         self.account_id = 0
@@ -59,9 +57,7 @@ class MajsoulConnection:
             return False
         resp = cast(
             liblq.ResCommon,
-            await self.rpc_call(
-                ".lq.Lobby.heatbeat", {"no_operation_counter": 0}
-            ),
+            await self.rpc_call(".lq.Lobby.heatbeat", {"no_operation_counter": 0}),
         )
         if resp.error.code:
             return False
@@ -113,9 +109,7 @@ class MajsoulConnection:
                         msg += f" {rome_name} mod_id: {mode_id}\n"
                         msg += f"对局id: {active_state.playing.game_uuid}"
                         # save game_uuid
-                        game_record[active_state.playing.game_uuid] = (
-                            friend.account_id
-                        )
+                        game_record[active_state.playing.game_uuid] = friend.account_id
                     elif not active_state.playing and friend.playing:
                         with open("game_record.json", encoding="utf8") as f:
                             game_record = json.load(f)
@@ -128,9 +122,7 @@ class MajsoulConnection:
                         msg += f"牌谱为 {url}\n"
 
                         # also save game_uuid
-                        game_record[friend.playing.game_uuid] = (
-                            friend.account_id
-                        )
+                        game_record[friend.playing.game_uuid] = friend.account_id
                     with open("game_record.json", "w", encoding="utf8") as f:
                         json.dump(game_record, f)
                     # set friend state
@@ -221,10 +213,11 @@ class MajsoulConnection:
                             f"Error: friend {data.account_id} already in self.friends"
                         )
                         msg = "Error: friend already in self.friends, please check .lq.NotifyFriendChange"
-                # maybe add friend
-                friend = MajsoulFriend(data.friend)
-                self.friends.append(friend)
-                msg = f"Successfully add friend {friend.nickname}"
+                else:
+                    # maybe add friend
+                    friend = MajsoulFriend(data.friend)
+                    self.friends.append(friend)
+                    msg = f"Successfully add friend {friend.nickname}"
             else:
                 # check if friend is in self.friends
                 for friend in self.friends:
@@ -457,8 +450,7 @@ async def createMajsoulConnection(access_token: str):
 
     serverListUrl = random.choice(ipDef.region_urls).url
     serverListUrl += (
-        "?service=ws-gateway&protocol=ws&ssl=true&rv="
-        + str(random.random())[2:]
+        "?service=ws-gateway&protocol=ws&ssl=true&rv=" + str(random.random())[2:]
     )
 
     resp = await AsyncClient(headers=HEADERS).get(serverListUrl)
@@ -520,9 +512,7 @@ class MajsoulManager:
                     self.conn.append(conn)
                     await conn.fetchInfo()
             else:
-                return (
-                    "❌错误: 未找到有效的ACCESS_TOKEN！请先进行[雀魂添加账号]"
-                )
+                return "❌错误: 未找到有效的ACCESS_TOKEN！请先进行[雀魂添加账号]"
         return self.conn[0]
 
     async def restart(self):
