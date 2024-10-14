b31b2fa12cd4292e84467b1d08264afd9e141eda
baiqwerdvd <158065462+baiqwerdvd@users.noreply.github.com>
2024-10-14 14:56:35 +0800

    update

--- MajsoulUID/majs_fu/__init__.py
+++ MajsoulUID/majs_fu/__init__.py
@@ -1,10 +1,10 @@
 import asyncio
 
-from gsuid_core.sv import SV
-from gsuid_core.bot import Bot
 from async_timeout import timeout
-from gsuid_core.models import Event
+from gsuid_core.bot import Bot
 from gsuid_core.logger import logger
+from gsuid_core.models import Event
+from gsuid_core.sv import SV
 from gsuid_core.utils.image.convert import convert_img
 
 from .fu import MahjongScoring
@@ -20,7 +20,7 @@ async def send_help_img(bot: Bot, ev: Event):
     image = await mahjong_scoring.set_answer()
 
     await bot.send(
-        "现在开始算符小游戏，你有60秒的时间回答问题，只需要回答数字即可。"
+        "现在开始算符小游戏，你有60秒的时间回答问题，只需要回答数字即可，你可以发送取消来中止游戏。"
     )
     await bot.send(await convert_img(image))
     try:
@@ -28,13 +28,22 @@ async def send_help_img(bot: Bot, ev: Event):
             while True:
                 resp = await bot.receive_mutiply_resp()
                 if resp is not None:
+                    if resp.text == "取消":
+                        await bot.send("游戏已取消！")
+                        return
                     if not resp.text.isdigit():
                         await bot.send("请输入数字！")
                         continue
                     result = await mahjong_scoring.check_answer(int(resp.text))
                     if not result:
-                        await bot.send("回答错误，请重新输入！")
-                        continue
+                        await bot.send("回答错误，正确答案是：")
+                        await bot.send(
+                            await convert_img(
+                                await mahjong_scoring.draw_problem_image(
+                                    include_answer=True
+                                )
+                            )
+                        )
                     await bot.send("回答正确！")
                     await bot.send(await convert_img(result))
                     break
