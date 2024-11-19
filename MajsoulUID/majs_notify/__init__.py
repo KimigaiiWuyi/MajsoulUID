301f74efdb56958ebdf96a8e01138176f751d007
baiqwerdvd <158065462+baiqwerdvd@users.noreply.github.com>
2024-11-19 16:40:36 +0800

    适配后端改动

--- MajsoulUID/majs_notify/__init__.py
+++ MajsoulUID/majs_notify/__init__.py
@@ -1,23 +1,23 @@
-import time
-import random
 import asyncio
+import random
+import time
 from typing import Dict
 from urllib.parse import parse_qs, urlparse
 
-import httpx
 import email_validator
-from gsuid_core.sv import SV
+import httpx
 from gsuid_core.bot import Bot
-from gsuid_core.models import Event
 from gsuid_core.logger import logger
+from gsuid_core.models import Event
+from gsuid_core.sv import SV
 from gsuid_core.utils.database.api import get_uid
 
-from .majsoul import manager
-from .constants import USER_AGENT
-from ..utils.error_reply import UID_HINT
 from ..utils.api.remote import encode_account_id2
-from .draw_friend_rank import draw_friend_rank_img
 from ..utils.database.models import MajsBind, MajsPush, MajsUser
+from ..utils.error_reply import UID_HINT
+from .constants import USER_AGENT
+from .draw_friend_rank import draw_friend_rank_img
+from .majsoul import manager
 
 majsoul_notify = SV("雀魂推送服务", pm=0)
 majsoul_friend_level_billboard = SV("雀魂好友排行榜")
@@ -124,22 +124,23 @@ async def majsoul_review_command(bot: Bot, ev: Event):
         response = await sess.get(f"{url}/review/{task_id}")
         response.raise_for_status()
         res = response.json()
-        if res.get("status") == "working" or res.get("status") == "pending":
+        status = res["status"]
+        if status == "working" or status == "pending":
             logger.info(f"Review Task {task_id} is working...")
             await asyncio.sleep(1)
-        elif res.get("review"):
+        elif status == "finished":
+            logger.info(f"Review Task {task_id} is finished!")
             break
     else:
         return await bot.send("❌ 未找到有效的Review信息!")
 
-    rating: float = res["review"]["rating"] * 100
-    matches_total = (
-        res["review"]["total_matches"] / res["review"]["total_reviewed"]
-    ) * 100
+    review_data = res["result"]["review"]
+    rating: float = review_data["rating"] * 100
+    matches_total = (review_data["total_matches"] / review_data["total_reviewed"]) * 100
     bad_move_up_count = 0
     bad_move_down_count = 0
 
-    for kyoku in res["review"]["kyokus"]:
+    for kyoku in review_data["kyokus"]:
         # cur_kyoku = kyoku["kyoku"]
         # cur_honba = kyoku["honba"]
 
@@ -165,10 +166,10 @@ async def majsoul_review_command(bot: Bot, ev: Event):
     bad_move_count = bad_move_up_count + bad_move_down_count
 
     Rating = f"{rating:.3f}"
-    total_matches = f"{res['review']['total_matches']}"
-    total_reviewed: int = res['review']['total_reviewed']
+    total_matches = f"{review_data['total_matches']}"
+    total_reviewed: int = review_data["total_reviewed"]
     matches = f"{total_matches}/{total_reviewed}"
-    total = f'{matches_total:.3f}%'
+    total = f"{matches_total:.3f}%"
 
     bad_move_ratio = f"{bad_move_count}/{total_reviewed}"
     bad_move_percent = f"{(bad_move_count / total_reviewed) * 100:.3f}%"
@@ -182,9 +183,7 @@ async def majsoul_review_command(bot: Bot, ev: Event):
     )
 
 
-@majsoul_yostar_login.on_command(
-    ("登录美服", "登录日服", "登陆日服", "登陆美服")
-)
+@majsoul_yostar_login.on_command(("登录美服", "登录日服", "登陆日服", "登陆美服"))
 async def majsoul_jp_login_command(bot: Bot, ev: Event):
     url = "https://passport.mahjongsoul.com/account/auth_request"
     headers = {
@@ -306,9 +305,7 @@ async def majsoul_add_at(bot: Bot, ev: Event):
             0,
         )
         if isinstance(connection, bool):
-            return await bot.send(
-                "❌ 登陆失败, 请输入正确的username和password!"
-            )
+            return await bot.send("❌ 登陆失败, 请输入正确的username和password!")
     else:
         return await bot.send(f"❌ 登陆失败!参考命令:\n{EXSAMPLE}")
 
@@ -365,9 +362,7 @@ async def majsoul_cancel_notify_command(bot: Bot, ev: Event):
             )
             if retcode == 0:
                 logger.success(f"[majs] {uid}订阅推送成功！当前值：{push_id}")
-                return await bot.send(
-                    f"[majs] 修改推送订阅成功！当前值：{push_id}"
-                )
+                return await bot.send(f"[majs] 修改推送订阅成功！当前值：{push_id}")
             else:
                 return await bot.send("[majs] 推送订阅失败！")
     else:
